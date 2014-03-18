27e2dba917f910edbb5222593ced2b8ab16f669a
unknown <greg.hovanesyan@gmail.com>
2014-03-18 13:32:34 +0400

    final

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 
 	<title>Barcamp logo</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<script type="text/javascript" src="cssrefresh.js"></script>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.2/angular.min.js"></script>	
 	<script type="text/javascript" src="perfection.js"></script>
 </head>
 <body onload="drawShape();">
