322bc1e99863822cf858b94940cbabc471b38afa
unknown <greg.hovanesyan@gmail.com>
2014-03-18 13:16:41 +0400

    final

--- index.php
+++ index.php
@@ -9,10 +9,16 @@
 	<script type="text/javascript" src="perfection.js"></script>
 </head>
 <body onload="drawShape();">
-<canvas id="canvas" height="257" width="294"></canvas>
-<div id="logosh-mogosh">
-</div>
 
+	<div id="container">
+		<canvas id="canvas" height="257" width="294"></canvas>
+		<div id="logosh-mogosh"></div>
+		<div class="barcamp">
+			<div class="yerevan">yerevan</div>
+			<span class="bar">bar</span><span class="camp">camp</span>
+		</div>
+		<div class="clear"></div>
+	</div>
 
 </body>
 
